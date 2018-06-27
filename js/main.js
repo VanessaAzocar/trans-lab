94859ec33a77ce0f98806bb8b9e02b1afe69eddd
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-27 11:22:46 -0300

    creando login

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-/* Holiii acá va tu código también */
+/* funcion de Ingresar contraseña */
+function enter() {
+  if (document.form.inputPassword.value.lenght >= ) {
+    const btnhide = document.getElementById('btn-primary');
+    btnhide.addEventListener('click', () => {
+      document.getElementsByClassName('submit')[0].classList.add('btn-primary');
+      document.getElementsByClassName('afterEntering')[0].classList.remove('afterEntering');
+    }
+    );
+  } else {
+    document.getElementById('incorrectPassword').innerHTML = '¡Datos inválidos! Vuelve a intentarlo.';
+  }
+}
